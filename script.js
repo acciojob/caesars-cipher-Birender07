65f50ac5aff551b77e132e8a211ed15f55dd215d
Birender07 <120732252+Birender07@users.noreply.github.com>
2023-06-06 11:35:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,15 +13,18 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = [];
 	for(let i =0;i<encodedStr.length;i++){
-		const asciiCode = character.charCodeAt(i);
-	    if(asciiCode<91 && asciiCode>64){
-		asciiCode = asciiCode+13;
-		const character = String.fromCharCode(asciiCode);
-		decodedArr.push(character)
-		}
-		else{
-			decodedArr.push(String.fromCharCode(asciiCode))
-		}
+		const char = lookup.encodedStr.charAt(i)
+        decodedArr.push(char)
+		
+		// const asciiCode = character.charCodeAt(i);
+	 //    if(asciiCode<91 && asciiCode>64){
+		// asciiCode = asciiCode+13;
+		// const character = String.fromCharCode(asciiCode);
+		// decodedArr.push(character)
+		// }
+		// else{
+		// 	decodedArr.push(String.fromCharCode(asciiCode))
+		// }
 	}
     return decodedArr;
 }
